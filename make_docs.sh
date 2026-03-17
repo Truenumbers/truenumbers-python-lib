40075078d7bef728d1c8bece57228f11743e897b
Tyler Oliver <toliver@truenumbers.com>
2026-03-17 11:02:21 -0400

    html

--- make_docs.sh
+++ make_docs.sh
@@ -4,4 +4,4 @@ python -m pydoc -w src.TruenumbersRestApi
 python -m pydoc -w src.TruenumbersTriggerApi
 python -m pydoc -w src.TruenumbersArtifactApi
 mv *.html docs
-python convert_pydoc_to_md.py
+# python convert_pydoc_to_md.py
